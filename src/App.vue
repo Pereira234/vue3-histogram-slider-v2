0c8710d3e567de24fd578d1a11ef678a12b5fe8f
Pereira234 <pedro.cl@webcomum.com>
2024-04-24 16:21:33 +0100

    added tests, and ran yarn install

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    original
     <HistogramSlider
       style="margin: 200px auto"
       type="double"
@@ -16,14 +17,14 @@
       :grid-num="2"
     />
 
+    fork test
     <HistogramSlider
       style="margin: 200px auto"
       ref="hist"
       type="double"
       :width="900"
       :bar-height="110"
-      :data="data"
-      :prettify="prettify"
+      :data="data_test"
       :clip="true"
       :drag-interval="true"
       :hideFromTo="true"
@@ -31,6 +32,8 @@
       :force-edges="false"
       :colors="['#4facfe', '#00f2fe']"
       v-slot="{ reset }"
+      :from="4"
+      :to="8"
       resettable
     >
       <button @click="reset">Ah</button>
@@ -44,6 +47,7 @@ export default {
   data() {
     return {
       data: require('../resources/data.json').map((d) => new Date(d).valueOf()),
+      data_test: [1, 2, 3, 4, 5, 6, 10, 2, 3, 4, 6, 7, 8, 9, 1, 1, 1],
       prettify: function (ts) {
         return new Date(ts).toLocaleDateString('en', {
           year: 'numeric',
